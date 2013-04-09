4bdfdeafb37fa772125c8287c4740396cdf46304
dfremont <Damien@Damien-PC>
2013-04-09 15:05:28 +0200

    wrong test example

---
+++ 20120227-junit3_to_junit4/src/test/java/com/dfremont/blog/JUnit4WrongTest.java
@@ -0,0 +1,34 @@
+package com.dfremont.blog;
+
+import org.junit.Test;
+
+public class JUnit4WrongTest {
+	@Test
+	public void testDivideValue() {
+		// Arrange
+		ClassToTest classToTest = new ClassToTest();
+		int param1 = 10;
+		int param2 = 2;
+		// Act
+		int result = classToTest.divide(param1, param2);
+		// Assert
+		// assertEquals(5, result);
+	}
+
+	@Test
+	public void testDivideValueWith0() {
+		// Arrange
+		ClassToTest classToTest = new ClassToTest();
+		int param1 = 10;
+		int param2 = 0;
+		// Act
+		try {
+			classToTest.divide(param1, param2);
+			// fail("Expected error!");
+		} catch (ArithmeticException e) {
+			// Assert
+			// assertEquals("Wrong Exc msg!", "Division by zero prohibited!",
+			// e.getMessage());
+		}
+	}
+}
