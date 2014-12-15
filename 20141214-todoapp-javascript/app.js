466328ac01318776c5bd1198ac93930b65f1d2bb
Damien Fremont <damien.fremont@gmail.com>
2014-12-15 09:08:35 +0100

    todoapp - create

---
+++ 20141214-todoapp-javascript/app.js
@@ -0,0 +1,32 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , todoRepository = require('./routes/todoRepository')
+  , todoService = require('./routes/todoService')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/api/todos', todoService.list);
+app.put('/api/todos', todoService.save);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
