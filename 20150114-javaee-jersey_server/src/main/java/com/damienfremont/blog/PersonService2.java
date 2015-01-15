a53158d419cc8dd7a96f2e34e818dd5ff5983aa0
Damien Fremont <damien.fremont@gmail.com>
2015-01-15 13:41:50 +0100

    jerseyserver - add big service

--- 20150114-javaee-jersey_server/src/main/java/com/damienfremont/blog/PersonService2.java
+++ 20150114-javaee-jersey_server/src/main/java/com/damienfremont/blog/PersonService2.java
@@ -3,17 +3,11 @@ package com.damienfremont.blog;
 import javax.ws.rs.Path;
 
 @Path("/todos")
-public class PersonService_level2 {
-
-	private PersonRepository dao = new PersonRepository();
+public class PersonService2 {
 
 	// TODO validation
 
 	// TODO return code
 
-	// TODO json
-
-	// TODO xml
-	
 	// creation 200 ou 204 à la seconde si on a répondu 201
 }
