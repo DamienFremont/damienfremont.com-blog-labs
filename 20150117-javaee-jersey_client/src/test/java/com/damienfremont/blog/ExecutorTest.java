31f541a717a77a2b4e66fcc04df33208a472ca6d
Damien Fremont <damien.fremont@gmail.com>
2015-01-18 04:50:16 +0100

    jerseyclient - fix imports

--- 20150117-javaee-jersey_client/src/test/java/com/damienfremont/blog/ExecutorTest.java
+++ 20150117-javaee-jersey_client/src/test/java/com/damienfremont/blog/ExecutorTest.java
@@ -4,20 +4,13 @@ import static java.util.concurrent.TimeUnit.SECONDS;
 import static net.jadler.Jadler.closeJadler;
 import static net.jadler.Jadler.initJadler;
 import static net.jadler.Jadler.onRequest;
-import static org.fest.assertions.api.Assertions.assertThat;
 import static org.junit.Assert.fail;
-
-import java.net.SocketTimeoutException;
-import java.nio.charset.Charset;
-import java.util.concurrent.TimeUnit;
-
 import net.jadler.Jadler;
 
 import org.fest.assertions.api.Assertions;
 import org.jboss.resteasy.client.ProxyFactory;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class ExecutorTest {
