9cd579ff1ed111497b17e0ed6b6f6cf20cc20a02
dfremont <damien.fremont@gmail.com>
2015-07-22 22:25:25 +0200

    bdd - maven modules fix

---
+++ 20150722-test-cucumber_scenario_reuse/project-core/src/test/java/com/damienfremont/blog/RunBDDTest.java
@@ -0,0 +1,12 @@
+package com.damienfremont.blog;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+// THIS CLASS RUN ALL THE TESTS SCENARIO
+
+@RunWith(Cucumber.class)
+public class RunBDDTest {
+
+}
