1df0e9e3ceb262826cbcd49586443826c892f397
Damien FREMONT <damien.fremont.job@gmail.com>
2015-11-05 00:44:08 +0100

    jab scroll tabs

--- 20151104-javaee-angularjs-bootstrap-infinite_scroll/src/main/webapp/app.js
+++ 20151104-javaee-angularjs-bootstrap-infinite_scroll/src/main/webapp/app.js
@@ -19,22 +19,22 @@ app.controller('MainCtrl', function ($scope, Service) {
   // GET PAGE
   function getAPage(start, number) {
     $scope.busy = true;
-	Service.get({
-	  page : 1+(start/number),
-	  size : number
-	  },
-	  function(pageable) {
-	    $scope.items = $scope.items.concat(pageable.content);
+  Service.get({
+    page : 1+(start/number),
+    size : number
+    },
+    function(pageable) {
+      $scope.items = $scope.items.concat(pageable.content);
         lastStart = start+number;
         $scope.busy = false;
-	    });
+      });
   }
 
   
   // SCROLL
   
   $scope.addMoreItems = function() {
-	  getAPage(lastStart, itemsByPage);
+    getAPage(lastStart, itemsByPage);
   };
   
 });
