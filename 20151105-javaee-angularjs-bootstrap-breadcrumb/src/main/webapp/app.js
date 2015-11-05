9ecc803e6082c384ca80c277781c305621f70c42
Damien FREMONT <damien.fremont@gmail.com>
2015-11-05 13:50:30 +0100

    jab breadcrumb - add router

--- 20151105-javaee-angularjs-bootstrap-breadcrumb/src/main/webapp/app.js
+++ 20151105-javaee-angularjs-bootstrap-breadcrumb/src/main/webapp/app.js
@@ -1,11 +1,36 @@
 'use strict';
 
-var app = angular.module(
-  'app', 
-  [ 'ui.router',
-    'ncy-angular-breadcrumb'
-    ]);
+var app = angular.module('app', [ 'ngRoute' ]);
 
-app.controller('MainCtrl', function ($scope) {
+app.config(function($routeProvider) {
+  $routeProvider //
+  .when('/page1', {
+    controller : 'MainCtrl',
+    templateUrl : 'views/page1.html',
+    breadcrumbs : [ home, page1 ]
+  }).when('/page2', {
+    controller : 'MainCtrl',
+    templateUrl : 'views/page2.html',
+    breadcrumbs : [ home, page2 ]
+  }).when('/page2/subpage', {
+    controller : 'MainCtrl',
+    templateUrl : 'views/page2subpage.html',
+    breadcrumbs : [ home, page2, page2subpage ]
+  }).otherwise({
+    templateUrl : 'views/home.html',
+    breadcrumbs : [ home ]
+  });
+});
+
+const home = { href : '#/', label : 'Home' };
+const page1 = { href : '#/page1', label : 'Page 1' };
+const page2 = { href : '#/page2', label : 'Page 2' };
+const page2subpage = { href : '#/page2/subpage', label : 'SubPage' };
+
+app.controller('BreadcrumbsController', function($scope, $route) {
+  $scope.route = $route;
+});
+
+app.controller('MainCtrl', function($scope) {
 
 });
--- 20151105-javaee-angularjs-bootstrap-breadcrumb/src/main/webapp/index.jsp
+++ 20151105-javaee-angularjs-bootstrap-breadcrumb/src/main/webapp/index.jsp
@@ -5,9 +5,7 @@
 <link rel="stylesheet" href="webjars/bootstrap/${bootstrap.version}/css/bootstrap.css">
 <!-- LIBS JS -->
 <script src="webjars/angularjs/${angularjs.version}/angular.js"></script>
-<script src="webjars/angularjs/${angularjs.version}/angular-resource.js"></script>
-<script src="webjars/angular-ui-router/${angular-ui-router.version}/release/angular-ui-router.js"></script>
-<script src="webjars/angular-breadcrumb/${angular-breadcrumb.version}/dist/angular-breadcrumb.js"></script>
+<script src="webjars/angularjs/${angularjs.version}/angular-route.js"></script>
 <!-- YOUR JS -->
 <script src="app.js"></script>
 </head>
@@ -15,10 +13,7 @@
 <div class="container">
 <h1>Breadcrumb</h1>
 
-<div ui-view></div>
-<!-- We'll also add some navigation: -->
-<a ui-sref="state1">State 1</a>
-<a ui-sref="state2">State 2</a>
+<div ng-view></div>
 
 </div>
 </body>
