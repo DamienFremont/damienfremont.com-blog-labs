14cbf09cd2ba4959bba783f020cb0cff1de0912e
Damien FREMONT <damien.fremont.job@gmail.com>
2015-11-07 02:52:02 +0100

    jab lang

--- 20151106-javaee-angularjs-bootstrap-multilingual/src/main/webapp/app.js
+++ 20151106-javaee-angularjs-bootstrap-multilingual/src/main/webapp/app.js
@@ -5,51 +5,54 @@ var app = angular.module('app',
     'pascalprecht.translate',
     'tmh.dynamicLocale']);
 
-var translationsEN = {
-  'FOO': 'This is a paragraph.'
-}
-var translationsFR = {
-  'FOO': 'C\'est un paragraph.'
-}
-*
 // i18n: LANGUAGE
 app.config( function ($translateProvider) {
   // ADD LANG
-  $translateProvider.useStaticFilesLoader({
+  $translateProvider
+  .useStaticFilesLoader({
     prefix: 'i18n/locale-',
     suffix: '.json'
-  });
-  $translateProvider.preferredLanguage('en');
+  })
   // REMEMBER
-  $translateProvider.useCookieStorage();
+  .preferredLanguage('en')
+  .fallbackLanguage('en')
+  .useSanitizeValueStrategy('escape')
+  .useCookieStorage();
 });
 
 // l10n: LOCALE
 app.config(function(tmhDynamicLocaleProvider) {
-  const localeLocationPattern = 'webjars/angular-i18n/1.4.7/angular-locale_{{locale}}.js';
-  tmhDynamicLocaleProvider.localeLocationPattern(localeLocationPattern);
-  tmhDynamicLocaleProvider.useCookieStorage();
+  const locales = 'webjars/angular-i18n/1.4.7/angular-locale_{{locale}}.js';
+  tmhDynamicLocaleProvider
+  .localeLocationPattern(locales)
+  .useCookieStorage();
 })
 
-app.controller('MainCtrl', function($scope, $translate, tmhDynamicLocale, $rootScope, $locale) {
-  $rootScope.availableLocales = {
+app.controller('MainCtrl', function($window, $scope, $translate, $cookieStore, tmhDynamicLocale, tmhDynamicLocaleCache, $rootScope, $locale) {
+  
+  $scope.availableLocales = {
     'en-us': 'English (US)',
     'en-gb': 'English (GB)',
     'fr-fr': 'French (FR)'
   };
-
+  
   // CHANGE LANG
   $scope.changeLocale = function(key) {
-    var keys = key.split("-");
-	var langKey = keys[0];
+    $rootScope.model = {selectedLocale: key};
+	// i18n
+	var langKey = key.substring(0,2);
     $translate.use(langKey);
+    // l10n
     tmhDynamicLocale.set(key);
   }
   
   // INIT
-  $rootScope.model = {selectedLocale: 'en'};
   $rootScope.$locale = $locale;
-  $rootScope.changeLocale = tmhDynamicLocale.set;
+  var lang = $cookieStore.get('tmhDynamicLocale.locale');;
+  if(!lang) {
+	  lang = $window.navigator.language || $window.navigator.userLanguage;
+  }
+  $scope.changeLocale(lang);
   
 });
 
