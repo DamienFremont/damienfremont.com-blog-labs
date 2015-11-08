3996ad1f84d158ca1823f9302ca2cba2479f14ef
Damien FREMONT <damien.fremont.job@gmail.com>
2015-11-08 21:15:12 +0100

    jab charts - real time

--- 20151108-javaee-angularjs-bootstrap-charts_d3js/src/main/webapp/app.js
+++ 20151108-javaee-angularjs-bootstrap-charts_d3js/src/main/webapp/app.js
@@ -154,3 +154,54 @@ app.controller('BarCtrl', function($scope) {
 	                                      }
 	                                  ];
 });
+
+app.controller('RealTimeCtrl', function($scope) {
+//    $scope.refreshInterval = 5;
+
+	   $scope.fetchData = function() {
+           var sin = [], cos = [];
+           for (var i = 0; i < 100; i++) {
+               sin.push({x: i, y: Math.sin(i/Math.random())});
+               cos.push({x: i, y: .5 * Math.cos(i/Math.random())});
+           }
+           return [
+               {
+                   values: sin,
+                   key: 'Sine Wave',
+                   color: '#ff7f0e'
+               },
+               {
+                   values: cos,
+                   key: 'Cosine Wave',
+                   color: '#2ca02c'
+               }
+           ];
+       }
+       $scope.xFunction = function(){
+           return function(d){
+               return d.x;
+           }
+       }
+       $scope.yFunction = function(){
+           return function(d){
+               return d.y;
+           }
+       }
+       
+       var refreshInterval = 1*1000;
+
+       $scope.exampleData = $scope.fetchData();
+       setInterval(function(){
+           $scope.$apply(function(){
+               var data = $scope.exampleData;
+               var i = (data[1].values[data[1].values.length -1].x + 1);
+               data[0].values.shift();
+               data[1].values.shift();
+               data[0].values.push({x: i , y: Math.sin(i/Math.random())});
+               data[1].values.push({x: i, y: .5 * Math.cos(i/Math.random())});
+               //var data = $scope.fetchData();
+               $scope.exampleData = data;
+           })
+       }, refreshInterval);
+	
+});
