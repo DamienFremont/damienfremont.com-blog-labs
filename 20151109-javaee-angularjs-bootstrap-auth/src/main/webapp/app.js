aa440dc151e31a6f56d64845708691c66cec3062
Damien FREMONT <damien.fremont.job@gmail.com>
2015-11-11 20:04:26 +0100

    jab auth - add templates

--- 20151109-javaee-angularjs-bootstrap-auth/src/main/webapp/app.js
+++ 20151109-javaee-angularjs-bootstrap-auth/src/main/webapp/app.js
@@ -2,22 +2,39 @@
 
 var app = angular.module('app', [ 'ngResource', 'ui.router', 'satellizer' ]);
 
-// BAR *************
+myApp.config(function($routeProvider) {
+  $routeProvider
+  // ROUTE FOR THE READ PAGE
+  .when('/', {
+    templateUrl : 'templates/home.html',
+    controller  : 'PersonCtrl'
+  })
+  // ROUTE FOR THE EDIT PAGE
+  .when('/login', {
+    templateUrl : 'templates/login.html',
+    controller  : 'PersonCtrl'
+  })
+  // DEFAULT
+  .otherwise({
+    redirectTo: '/'
+  });
+});
+
 app.config(function($authProvider) {
-	
-	$authProvider.withCredentials = true;
-	$authProvider.tokenRoot = null;
-	$authProvider.cordova = false;
-	$authProvider.baseUrl = './api/';
-	$authProvider.loginUrl = '/auth/login';
-	$authProvider.signupUrl = '/auth/signup';
-	$authProvider.unlinkUrl = '/auth/unlink/';
-	$authProvider.tokenName = 'token';
-	$authProvider.tokenPrefix = 'satellizer';
-	$authProvider.authHeader = 'Authorization';
-	$authProvider.authToken = 'Bearer';
-	$authProvider.storageType = 'localStorage';
-	
+  
+  $authProvider.withCredentials = true;
+  $authProvider.tokenRoot = null;
+  $authProvider.cordova = false;
+  $authProvider.baseUrl = './api/';
+  $authProvider.loginUrl = '/auth/login';
+  $authProvider.signupUrl = '/auth/signup';
+  $authProvider.unlinkUrl = '/auth/unlink/';
+  $authProvider.tokenName = 'token';
+  $authProvider.tokenPrefix = 'satellizer';
+  $authProvider.authHeader = 'Authorization';
+  $authProvider.authToken = 'Bearer';
+  $authProvider.storageType = 'localStorage';
+  
 //    $stateProvider
 //    .state('home', {
 //      url: '/',
@@ -58,23 +75,23 @@ app.config(function($authProvider) {
 });
 
 app.factory('Service', function($resource) {
-	return $resource('api/datas/values');
+  return $resource('api/datas/values');
 });
 
 app.controller('LoginCtrl', function($scope, $auth) {
 
-	$scope.email = 'm';
-	$scope.password = 'p';
-	
-	var user = {
-		email : $scope.email,
-		password : $scope.password
-	};
+  $scope.email = 'm';
+  $scope.password = 'p';
+  
+  var user = {
+    email : $scope.email,
+    password : $scope.password
+  };
 
-	$auth.login(user).then(function(response) {
-		// Redirect user here after a successful log in.
-	}).catch(function(response) {
-		// Handle errors here, such as displaying a notification
-		// for invalid email and/or password.
-	});
+  $auth.login(user).then(function(response) {
+    // Redirect user here after a successful log in.
+  }).catch(function(response) {
+    // Handle errors here, such as displaying a notification
+    // for invalid email and/or password.
+  });
 });
