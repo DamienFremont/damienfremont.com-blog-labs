9207a82688b1f4b56a0c95caa87ffdd9fadbcdfb
Damien FREMONT <damien.fremont.job@gmail.com>
2015-11-12 00:29:34 +0100

    jab auth - add java lang ext for backend auth

--- 20151109-javaee-angularjs-bootstrap-auth/src/main/java/com/damienfremont/blog/ServiceAuth.java
+++ 20151109-javaee-angularjs-bootstrap-auth/src/main/java/com/damienfremont/blog/ServiceAuth.java
@@ -1,25 +1,43 @@
 package com.damienfremont.blog;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
+import org.glassfish.jersey.internal.Errors.ErrorMessage;
+import org.glassfish.jersey.message.internal.Token;
+
+import com.nimbusds.jose.JOSEException;
+
+import com.google.common.base.Optional;
 
 @Path("/auth")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class ServiceAuth {
 
   @Path("/login")
   @POST
-  @Consumes(MediaType.APPLICATION_JSON)
-  public void login(Object o) {
-    // TODO
-  }  
+  public Response login(@Valid final User user, @Context final HttpServletRequest request) throws JOSEException {
+    final Optional<User> foundUser = dao.findByEmail(user.getEmail());
+    if (foundUser.isPresent() && PasswordService.checkPassword(user.getPassword(), foundUser.get().getPassword())) {
+      final Token token = AuthUtils.createToken(request.getRemoteHost(), foundUser.get().getId());
+      return Response.ok().entity(token).build();
+    }
+    return Response.status(Status.UNAUTHORIZED).entity(new ErrorMessage(LOGING_ERROR_MSG)).build();
+  }
+  
+  class User {
+    String id;
+    String password;
+    String email;
+  }
 
 }
---
+++ 20151109-javaee-angularjs-bootstrap-auth/src/main/java/com/damienfremont/blog/Token.java
@@ -0,0 +1,15 @@
+package com.damienfremont.blog;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Token {
+  String token;
+
+  public Token(@JsonProperty("token") String token) {
+    this.token = token;
+  }
+
+  public String getToken() {
+    return token;
+  }
+}
