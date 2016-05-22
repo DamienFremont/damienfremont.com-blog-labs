80541dc03f5bf9603787b8768ad02685c90a9dfe
Damien FREMONT <damien.fremont@gmail.com>
2016-05-22 21:51:16 +0200

    fix

--- 20160522-javaee-angularjs-webpackaging_min_single_file/src/main/webapp/js/app.js
+++ 20160522-javaee-angularjs-webpackaging_min_single_file/src/main/webapp/js/app.js
@@ -14,12 +14,12 @@ angular.module('myapp', [ //
 
 	// PAGE1
 	.when('/page1', {
-		templateUrl : '/js2/page1/page1.html',
+		templateUrl : '/js/page1/page1.html',
 		controller : 'Page1Ctrl'
 	})
 	// PAGE 2
 	.when('/page2', {
-		templateUrl : '/js2/page2/page2.html',
+		templateUrl : '/js/page2/page2.html',
 		controller : 'Page2Ctrl'
 	})
 	// DEFAULT
