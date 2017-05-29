9a77a5e7900db7927ba07a6f795eb3ed03c0976c
Damien Frémont <damien.fremont@gmail.com>
2017-05-29 19:00:28 +0200

    20170529-js-react-material-express-setting_up_a_project - add react webpack

---
+++ 20170529-js-react-material-express-setting_up_a_project/views/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {test: 'foo'};
+  }
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+}
---
+++ 20170529-js-react-material-express-setting_up_a_project/webpack.config.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = [
+{
+  entry: './views/index.jsx',
+  output: {
+    path: path.resolve(__dirname, '/public/'),
+    filename: '[name].js',
+    publicPath: '/'
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'views/index.tpl.html',
+      inject: 'body',
+      filename: 'index.html'
+    })
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        "presets": ["react", "es2015", "stage-0"]
+      }
+    }, {
+      test: /\.json?$/,
+      loader: 'json'
+    }, {
+      test: /\.css$/,
+      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
+    }]
+  }
+}
+];
